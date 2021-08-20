2799b9845e48b517e4d5003ea8017d4e9df09254
Arjun Thaker <arjthaker@gmail.com>
2021-08-19 22:09:53 -0700

    added player movement and shooting projectile

---
+++ Assets/Scripts/MoveProjectile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveProjectile : MonoBehaviour
+{
+    public Rigidbody2D Projectile;
+
+    public float moveSpeed = 10.0f;
+    void Start()
+    {
+        Projectile = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Projectile.velocity = new Vector2(0, 1) * moveSpeed;
+    }
+}
