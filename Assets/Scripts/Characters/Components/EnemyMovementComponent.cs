aab1de8a943d57db82e4a2cf0c59d451047d3f8b
Nicolas Morales Escobar <nico96101@hotmail.com>
2021-08-21 13:36:31 -0700

    [GAL-23] Added movement component.

---
+++ Assets/Scripts/Characters/Components/EnemyMovementComponent.cs
@@ -0,0 +1,11 @@
+// Copyright (C) 2021, Galactic Whales. All rights reserved.
+
+using UnityEngine;
+
+public class EnemyMovementComponent : CharacterMovementComponent
+{
+    public override void Move()
+    {
+
+    }
+}
