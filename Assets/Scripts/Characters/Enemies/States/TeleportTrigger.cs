c55a01789c855e046e5b92a2299bae238fcc456e
Arjun Thaker <arjthaker@gmail.com>
2021-09-25 23:57:16 -0700

    Created enemy formation and teleportation function

---
+++ Assets/Scripts/Characters/Enemies/States/TeleportTrigger.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+// Start is called before the first frame update
+        [RequireComponent(typeof(Collider2D))]
+public class TeleportTrigger : MonoBehaviour
+{
+    public enum TriggerType { Enter, Exit };
+
+    [Tooltip("The Transform to teleport to")]
+    [SerializeField] Transform teleportTo; 
+
+    [Tooltip("The filter Tag")]
+    [SerializeField] string filterTag = "Player";
+
+    [Tooltip("Trigger Event to Teleport")]
+    [SerializeField] TriggerType type;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (type != TriggerType.Enter)
+            return;
+
+        if (filterTag == string.Empty || other.CompareTag(filterTag))
+            other.transform.position = teleportTo.position;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (type != TriggerType.Exit)
+            return;
+
+        if (filterTag == string.Empty || other.CompareTag(filterTag))
+            other.transform.position = teleportTo.position;
+    }
+}
+
+
+   
